8d38960de460f278aab14e1ec41deae04c5a0090
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-11 14:06:46 +0100

    made some changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,9 +13,12 @@ import { setMode } from "../state";
 import profileImage from "./../assets/profile.jpeg";
 import {
   AppBar,
+  Box,
+  Button,
   IconButton,
   InputBase,
   Toolbar,
+  Typography,
   useTheme,
 } from "@mui/material";
 
@@ -67,14 +70,43 @@ const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
             <SettingsOutlined sx={{ fontSize: "25px" }} />
           </IconButton>
 
-              <FlexBetween>
-                <Button onClick={handleClick}
-                  sx={{
-
-                  }}>
-
-                </Button>
-              </FlexBetween>
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                textTransform: "none",
+                gap: "1rem",
+              }}
+            >
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="32px"
+                width="32px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.85rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.8rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+            </Button>
+          </FlexBetween>
         </FlexBetween>
       </Toolbar>
     </AppBar>
