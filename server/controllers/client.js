7182a667e07241c087b26be17acd0d913699e061
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-14 12:33:54 +0100

    added a comment

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -4,7 +4,7 @@ import ProductStat from "../models/ProductStat";
 
 export const getProducts = async (req, res) => {
     try{
-
+        const products = await Product.find();
     } catch (error){
         res.status(404).json({message: error.message});
     }
