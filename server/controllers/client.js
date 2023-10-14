60b789ecc1c920554a548a63e1097769ad1d7c8c
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-14 12:37:51 +0100

    added some changes

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -5,6 +5,18 @@ import ProductStat from "../models/ProductStat";
 export const getProducts = async (req, res) => {
     try{
         const products = await Product.find();
+
+        const productsWithStat = await Promise.all(
+            products.map(async(product) => {
+                const stat = await ProductStat.find({
+                    productId: product._id,
+                })
+                return{
+                    ...product._doc,
+                    stat,
+                }
+            })
+        )
     } catch (error){
         res.status(404).json({message: error.message});
     }
