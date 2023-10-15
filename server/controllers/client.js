3745a0acff8992ad15cc98e5a344c4def3c4731e
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-15 13:01:09 +0100

    added some changes

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,5 @@
 import Product from "../models/Product.js";
-import ProductStat from "../models/ProductStat";
+import ProductStat from "../models/ProductStat.js";
 
 
 export const getProducts = async (req, res) => {
