95bdce7fbc086b77626921474df23a1c6de73df2
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-16 14:36:52 +0100

    added a comment

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import {
+    Typography,
+    Box,
+    useTheme
+} from '@mui/material';
+import React from 'react'
+
+const Header = ( title, subtitle) => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
