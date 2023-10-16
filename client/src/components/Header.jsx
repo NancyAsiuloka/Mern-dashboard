e0416484e513107986c90bf757ebdde37afce8aa
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-16 14:39:46 +0100

    made some changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,8 +6,21 @@ import {
 import React from 'react'
 
 const Header = ( title, subtitle) => {
+    const theme = useTheme();
   return (
-    <div>Header</div>
+    <Box>
+      <Typography
+        variant="h2"
+        color={theme.palette.secondary[100]}
+        fontWeight="bold"
+        sx={{ mb: "5px" }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="h5" color={theme.palette.secondary[300]}>
+        {subtitle}
+      </Typography>
+    </Box>
   )
 }
 
