b84e170f17a35eeef206f83e936151b16edd3e07
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-18 16:06:49 +0100

    added margin

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -20,7 +20,7 @@ const Products = () => {
 
   console.log("data", data);
 
-  return <Box>
+  return <Box m="1.5rem 2.5rem">
      <Header title="PRODUCTS" subtitle="See your list of products." />
     </Box>
 }
