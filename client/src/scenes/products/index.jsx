984e840bd7f7977b4ea5286261e7f9aa0d38a2f9
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-18 18:11:56 +0100

    made some changes

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -14,6 +14,39 @@ import {
 import Header from "../../components/Header";
 import { useGetProductsQuery } from "../../state/api";
 
+const Product = ({
+  _id,
+  name,
+  description,
+  price,
+  rating,
+  category,
+  supply,
+  stat
+}) => {
+  const theme = useTheme();
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <Card
+      sx={{
+        backgroundImage: "none",
+        backgroundColor: theme.palette.background.alt,
+        borderRadius: "0.55rem"
+      }}
+    >
+      <CardContent>
+        <Typography
+          sx={{ fontSize: 14}}
+          color={theme.palette.secondary[200]}
+          gutterBottom>
+            {category}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
+
 const Products = () => {
   const { data, isLoading } = useGetProductsQuery();
   const isNonMobile = useMediaQuery("(min-width: 1008px)");
@@ -31,9 +64,14 @@ const Products = () => {
           justifyContent="space-between"
           rowGap="20px"
           columnGap="1.33%"
-        ></Box>
+          sx={{
+            "& > div": { gridColumn: isNonMobile ? undefined : "span 4"}
+          }}
+        >
+          {data.map() => ()}
+        </Box>
       ) : (
-        <></>
+        <>Loading...</>
       )}
     </Box>
   );
