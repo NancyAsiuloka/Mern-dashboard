6a606c26b2a65c3f6bcc3ae96d0f0317a1ebe6df
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-18 17:53:42 +0100

    added some code

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -1,18 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import {
-    Box,
-    Card,
-    CardActions,
-    CardContent,
-    Collapse,
-    Button,
-    Typography,
-    Rating,
-    useTheme,
-    useMediaQuery,
-  } from "@mui/material";
-  import Header from '../../components/Header';
-  import { useGetProductsQuery } from "../../state/api";
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Collapse,
+  Button,
+  Typography,
+  Rating,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import Header from "../../components/Header";
+import { useGetProductsQuery } from "../../state/api";
 
 const Products = () => {
   const { data, isLoading } = useGetProductsQuery();
@@ -20,9 +20,23 @@ const Products = () => {
 
   console.log("data", data);
 
-  return <Box m="1.5rem 2.5rem">
-     <Header title="PRODUCTS" subtitle="See your list of products." />
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="PRODUCTS" subtitle="See your list of products." />
+      {data || !isLoading ? (
+        <Box
+          mt="20px"
+          display="grid"
+          gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+          justifyContent="space-between"
+          rowGap="20px"
+          columnGap="1.33%"
+        ></Box>
+      ) : (
+        <></>
+      )}
     </Box>
-}
+  );
+};
 
-export default Products
+export default Products;
