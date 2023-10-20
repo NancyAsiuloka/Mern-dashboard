02d9e506b550059752284018d9ba72ed26a2ebff
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-20 14:38:55 +0100

    added some changes

--- server/models/OverallStat.js
+++ server/models/OverallStat.js
@@ -5,7 +5,23 @@ const OverallStatSchema = new mongoose.Schema(
         totalCustomers: Number,
         yearlySalesTotal: Number,
         yearlyTotalSoldUnits: Number,
-        year: Number
+        year: Number,
+        monthlyData: [
+            {
+                month: String,
+                totalSales: Number,
+                totalUnits: Number
+            }
+        ],
+        dailyData: {
+            date: String,
+            totalSales: Number,
+            totalUnits: Number
+        },
+        salesByCategory: {
+            type: Map,
+            of: Number,
+        }
     },
     {timestamps: true}
 );
