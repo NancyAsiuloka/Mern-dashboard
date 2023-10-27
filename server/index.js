f02e0de0d343abf8d64f6b43c6260814df0b847a
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-27 22:07:37 +0100

    made some changes

--- server/index.js
+++ server/index.js
@@ -16,12 +16,14 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
-  dataOverallStat
+  dataOverallStat,
+  dataAffiliateStat
 } from "./data/index.js";
 
 // CONFIGURATION
@@ -52,6 +54,7 @@ mongoose
     app.listen(PORT, () => console.log(`Server port: ${PORT}`));
 
     // Only add data one time
+    // AffiliateStat.insertMany(dataAffiliateStat);
     // OverallStat.insertMany(dataOverallStat);
     // Product.insertMany(dataProduct)
     // ProductStat.insertMany(dataProductStat)
