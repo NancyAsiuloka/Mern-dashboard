0077b73bb7aa9a78a3232498d8d99c5482609a82
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-10-27 22:06:06 +0100

    rendered a component

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ import Daily from './scenes/daily';
 import Monthly from './scenes/monthly';
 import Breakdown from './scenes/breakdown';
 import Admin from './scenes/admin';
-
+import Performance from './scenes/performance';
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
@@ -37,6 +37,7 @@ function App() {
               <Route path="/monthly" element={<Monthly/>} />
               <Route path="/breakdown" element={<Breakdown/>} />
               <Route path="/admin" element={<Admin/>} />
+              <Route path="/performance" element={<Performance/>} />
           </Route>
         </Routes>
       </ThemeProvider>
