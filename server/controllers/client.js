b8f6cd1a47fd3e6790d906978e19a132b63793f1
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-11-17 23:41:09 +0100

    added a function

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -87,7 +87,7 @@ export const getGeography = async (req, res) => {
         const formattedLocations = Object.entries(mappedLocations).map(
             ([country, count]) => {
                 return { id: country, value: count}
-            }
+            };
         );
         res.status(200).json(formattedLocations);
     } catch (error) {
